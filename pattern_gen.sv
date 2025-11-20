002e36260461a0cb3ec9dd48f25c32c9e5e777d1
Daniel Jarka <daniel.jarka@tufts.edu>
2025-11-19 20:21:03 -0500

    Updated Code to Display Background on VGA

--- pattern_gen.sv
+++ pattern_gen.sv
@@ -1,10 +1,11 @@
 module pattern_gen (
     input logic valid,
     input logic [9:0] col,  
-    input logic [9:0] row,    
+    input logic [9:0] row, 
+    input logic [5:0] tile,   
     output logic [5:0] rgb      
 );
 
-assign rgb = valid ? (row ^ col) % 32 : 6'd0;
+assign rgb = valid ? tile : 6'd0;
 
 endmodule 
--- top.sv
+++ top.sv
@@ -6,11 +6,17 @@ module top (
   output logic [5:0] rgb
 );
   
-// logic hsync;
-// logic vsync;
+
 logic valid;
 logic [9:0] col;
 logic [9:0] row;
+
+
+    // tile from ROM
+    logic [5:0] tile;
+
+    // ROM address
+    logic [16:0] addr;
 
   mypll u_mypll (
     .clock_in(clk_in),
@@ -26,10 +32,33 @@ logic [9:0] row;
     .valid(valid) 
   );
 
+    localparam int TILE_SIZE = 8;
+    localparam int TILES_X = 640 / TILE_SIZE;  // = 80
+
+    logic [6:0]   tile_x;
+    logic [6:0]   tile_y;
+    // logic [16:0]  rom_addr;
+    // logic [5:0]   rom_data;
+
+    // Determine which tile this pixel belongs to
+    assign tile_x = col[9:3];   // col / 8
+    assign tile_y = row[9:3];   // row / 8
+
+    // Compute address EXACTLY like Python script
+    assign addr = tile_y * TILES_X + tile_x;
+
+    // ROM instance
+    ROM_Screen u_rom (
+        .clk(clk_out),
+        .addr(addr),
+        .data(tile)
+    );
+
   pattern_gen u_pattern_gen (
     .valid(valid),
     .col(col),  
-    .row(row),    
+    .row(row),
+    .tile(tile),    
     .rgb(rgb)     
   );
 
