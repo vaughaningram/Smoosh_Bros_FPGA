a0e8f774dc3b9e0d8e348fa94d3de5e5e3dd4cd9
Ajsbreezy <ajsbreezy@gmail.com>
2025-11-20 14:36:12 -0500

    Create attack_FSM.sv

---
+++ State Machines/attack_FSM.sv
@@ -0,0 +1,15 @@
+module attack_FSM (
+    input logic btn_atk,
+    input logic btn_up,
+    input logic btn_down,
+    input logic btn_right,
+    input logic btn_left,
+    input logic hit_stun_active,
+    output logic attack_active,
+    output logic anim_ID,
+);
+//always_comb begin
+//if (hit_stun_active) 
+//end
+
+endmodule
