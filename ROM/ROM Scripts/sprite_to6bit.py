a1869cef85746df1c297bb4596ddf7aa26b18902
Ajsbreezy <ajsbreezy@gmail.com>
2025-12-02 17:33:30 -0500

    Script to go From 8 bit color to 6 Bits
    Helpful for some ROM stuff I want to do later

---
+++ ROM/ROM Scripts/sprite_to6bit.py
@@ -0,0 +1,23 @@
+from PIL import Image
+
+IMAGE = "../Sprites/marco_sprite-1.png (3).png"
+OUTPUT = "6_Bit_Output.png"
+
+img = Image.open(IMAGE).convert("RGB")
+
+pixel_data = list(img.getdata())
+
+new_img_data = []
+for r, g, b in pixel_data:
+    # shifting 6 bits to the right to convert to 6bit RGB then 6 bitd to the left
+    # to get back to standard 8 bit rgb for png
+    print(r, g, b)
+    # appending data
+    new_img_data.append(((r >> 6) << 6,(g >>6) << 6 ,(b >> 6) << 6))
+
+# saving image
+new_img = Image.new(img.mode, img.size)
+new_img.putdata(new_img_data)
+new_img.save(OUTPUT)
+
+
